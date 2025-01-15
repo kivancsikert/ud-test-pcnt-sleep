e19061c2fb8ad5fbaaaa4787476a579d8508b9da
Lóránt Pintér <lorant.pinter@gmail.com>
2025-01-15 12:06:44 +0100

    Turn a regular LED on when we are awake

--- main/main.cpp
+++ main/main.cpp
@@ -19,15 +19,24 @@ using namespace std::chrono;
 
 bool previousState = false;
 std::atomic<uint32_t> counter { 0 };
-std::atomic<int64_t> sleepDuration { 0 };
+std::atomic<uint64_t> sleepDurationInUs { 0 };
+std::atomic<uint32_t> sleepCount { 0 };
 
 esp_pm_lock_handle_t noSleep;
 
 bool timerRunning = false;
 esp_timer_handle_t timer = nullptr;
 
-static const gpio_num_t ledGpio = GPIO_NUM_8;
+static const gpio_num_t rgbLedGpio = GPIO_NUM_8;
+static const gpio_num_t ledGpio = GPIO_NUM_15;
 static const gpio_num_t buttonGpio = GPIO_NUM_9;
+
+// static tNeopixelContext neopixel = neopixel_Init(1, rgbLedGpio);
+// static tNeopixel off = { 0, NP_RGB(0, 0, 0) };
+// static tNeopixel red = { 0, NP_RGB(50, 0, 0) };
+// static tNeopixel green = { 0, NP_RGB(0, 50, 0) };
+// static tNeopixel blue = { 0, NP_RGB(0, 0, 50) };
+// static tNeopixel white = { 0, NP_RGB(17, 17, 17) };
 
 void timerCallback(void* arg) {
     ESP_LOGI("main", "Timer expired, allowing light sleep");
@@ -59,33 +68,49 @@ void interruptHandler(void* arg) {
 }
 
 extern "C" void app_main() {
-    tNeopixelContext neopixel = neopixel_Init(1, ledGpio);
-    tNeopixel pixel = { 0, NP_RGB(50, 0, 0) };
-    neopixel_SetPixel(neopixel, &pixel, 1);
+    // neopixel_SetPixel(neopixel, &red, 1);
+    // neopixel_SetPixel(neopixel, &green, 1);
+    // neopixel_SetPixel(neopixel, &blue, 1);
+    // neopixel_SetPixel(neopixel, &white, 1);
 
-    // esp_pm_sleep_cbs_register_config_t cbs_conf = {
-    //     .enter_cb = [](int64_t timeToSleepInUs, void* arg) {
-    //         return ESP_OK; },
-    //     .exit_cb = [](int64_t timeSleptInUs, void* arg) {
-    //         sleepDuration += timeSleptInUs;
-    //         return ESP_OK; },
-    // };
-    // ESP_ERROR_CHECK(esp_pm_light_sleep_register_cbs(&cbs_conf));
+    // Set up GPIO for the LED
+    gpio_config_t ledConfig = {
+        .pin_bit_mask = 1ULL << ledGpio,
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&ledConfig);
+    gpio_set_direction(ledGpio, GPIO_MODE_OUTPUT);
+    gpio_sleep_set_direction(ledGpio, GPIO_MODE_OUTPUT);
 
-    // esp_pm_config_t pm_config = {
-    //     .max_freq_mhz = CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ,
-    //     .min_freq_mhz = CONFIG_XTAL_FREQ,
-    //     .light_sleep_enable = true,
-    // };
-    // ESP_ERROR_CHECK(esp_pm_configure(&pm_config));
+    esp_pm_sleep_cbs_register_config_t cbs_conf = {
+        .enter_cb = [](int64_t timeToSleepInUs, void* arg) {
+            gpio_set_level(ledGpio, 1);
+            return ESP_OK; },
+        .exit_cb = [](int64_t timeSleptInUs, void* arg) {
+            gpio_set_level(ledGpio, 0);
+            sleepDurationInUs += timeSleptInUs;
+            sleepCount++;
+            return ESP_OK; },
+    };
+    ESP_ERROR_CHECK(esp_pm_light_sleep_register_cbs(&cbs_conf));
 
-    // esp_pm_lock_create(ESP_PM_NO_LIGHT_SLEEP, 0, "no-sleep", &noSleep);
+    esp_pm_config_t pm_config = {
+        .max_freq_mhz = CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ,
+        .min_freq_mhz = CONFIG_XTAL_FREQ,
+        .light_sleep_enable = true,
+    };
+    ESP_ERROR_CHECK(esp_pm_configure(&pm_config));
 
-    // esp_timer_create_args_t timer_args = {
-    //     .callback = &timerCallback,
-    //     .name = "end-sleep"
-    // };
-    // ESP_ERROR_CHECK(esp_timer_create(&timer_args, &timer));
+    esp_pm_lock_create(ESP_PM_NO_LIGHT_SLEEP, 0, "no-sleep", &noSleep);
+
+    esp_timer_create_args_t timer_args = {
+        .callback = &timerCallback,
+        .name = "end-sleep"
+    };
+    ESP_ERROR_CHECK(esp_timer_create(&timer_args, &timer));
 
     // gpio_install_isr_service(0);
 
@@ -105,11 +130,15 @@ extern "C" void app_main() {
 
     // gpio_isr_handler_add(buttonGpio, interruptHandler, nullptr);
 
+    auto startTime = high_resolution_clock::now();
     while (true) {
-        printf("Counter: %lu; slept %lld us\n",
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        auto endTime = high_resolution_clock::now();
+        auto delayDurationInUs = duration_cast<microseconds>(endTime - startTime).count();
+        ESP_LOGI("main", "Counter: %lu; awake %.3f%% (%lu cycles)",
             counter.exchange(0),
-            sleepDuration.exchange(0));
-        fflush(stdout);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+            (1.0 - ((double) sleepDurationInUs.exchange(0)) / ((double) delayDurationInUs)) * 100.0,
+            sleepCount.exchange(0));
+        startTime = endTime;
     }
 }
